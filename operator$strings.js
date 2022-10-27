6c71cb2d5bb52cb493e4e2bfe11cbfd46712d14d
SWADESH <swadesh.patra961@gmail.com>
2022-10-27 13:12:34 +0530

    operator and string

---
+++ operator$strings.js
@@ -0,0 +1,19 @@
+// assignment operator
+a = 4;
+a+=3;
+console.log(a)
+
+// arithmatic operator
+let x = 14;
+let b = 7;
+
+console.log(x+b); 
+console.log(x+b);
+console.log(x*b);
+console.log(x/b);
+console.log(x%b);
+console.log(x**b);
+
+// decrement operator
+// let x = 3;
+// console.log(--x);
