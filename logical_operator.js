98f7bc89c2e605d82062b52c0720e5195b4c380d
SWADESH <swadesh.patra961@gmail.com>
2022-10-28 20:19:47 +0530

    logical operator

---
+++ logical_operator.js
@@ -0,0 +1,23 @@
+// logical operator perform logical operation and return a boolean value either true or false
+
+// logical AND
+// console.log(true && true); // true
+// console.log(true && false); // false
+
+// // logical OR
+// console.log(true || false); // true
+// console.log(false || false);
+// console.log(false || true);
+// console.log(true || true);
+// // logical NOT
+// console.log(!true); // false
+// console.log(!false);
+
+// example
+
+console.log((2>3) && (3>2));
+console.log((2<3) && (4>2));
+console.log((2>3) || (3>2));
+console.log((2>3) || (1>2));
+console.log(!(4>2));
+console.log(!(4<2));
