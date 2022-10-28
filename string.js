b651a32d5b930da708dd3569d330c528d7ab7b9f
SWADESH <swadesh.patra961@gmail.com>
2022-10-28 21:25:18 +0530

    important topic in a string

---
+++ string.js
@@ -0,0 +1,17 @@
+let name = "raja";
+let name1 = "madan";
+let result = 'the words are "raja" and "madan"';
+console.log(result);
+
+let result = `the words are ${name} and ${name1}`;
+console.log(result);
+
+a = "hello";
+console.log(a[2]);
+console.log(a.charAt(2));
+
+// multiple line
+
+const a = 'this is a big file \,we cant read it \,we need time for it';
+console.log(a);
+
