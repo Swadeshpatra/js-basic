4102f3ae4a886e140c9252360db43bc7a51375fd
SWADESH <swadesh.patra961@gmail.com>
2022-10-28 19:50:44 +0530

    comparision operator

---
+++ comparision_operator.js
@@ -0,0 +1,17 @@
+// equal operator.
+console.log(2==2);
+console.log(2=='2');
+
+// not equal to operator
+console.log(3!=2);
+console.log('hello' != 'Hello');
+console.log(3!=3);
+//  strict equal operator
+
+console.log(2===2);
+console.log(2==='2');
+
+// strictly not equal operator
+
+console.log(2!=='2');
+console.log(2!==2);
