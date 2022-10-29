997d0c8d028d498f68f0b93c084242e66b2446ca
SWADESH <swadesh.patra961@gmail.com>
2022-10-29 20:24:50 +0530

    touppercase

---
+++ convert_upperclass.js
@@ -0,0 +1,7 @@
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = '     JavaScript    ';
+
+// converting the text to uppercase
+const result2 = text1.toUpperCase();
+console.log(result2); // HELLO
