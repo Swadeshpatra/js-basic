239d017e9f28bb6287abd9a6fecaff8c8d23b37b
SWADESH <swadesh.patra961@gmail.com>
2022-10-29 20:55:38 +0530

    slice

---
+++ slice.js
@@ -0,0 +1,6 @@
+let a = "unfriendly";
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.slice(6,9));
+console.log(a.concat("      ","people"));
+console.log(a.replace("unfriendly","web3"));
