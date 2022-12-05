3234682985315de8f2f02257950921064eb9d20f
SWADESH <swadesh.patra961@gmail.com>
2022-12-05 11:38:15 +0530

    switch

--- switch.js
+++ switch.js
@@ -1,27 +1,77 @@
-let day = 0;
-switch(day){
-    case 0:
-        console.log("sunday");
-        break;
-    case 1:
-        console.log("monday");
-        break;    
-    case 2:
-        console.log("tuesday");
-        break;
-    case 3:
-        console.log("wednesday");
-        break;
-    case 4:
-        console.log("thursday");
-        break; 
-    case 5:
-        console.log("friday");
-        break;
-    case 6:
-        console.log("saturday");
+// let day = 0;
+// switch(day){
+//     case 0:
+//         console.log("sunday");
+//         break;
+//     case 1:
+//         console.log("monday");
+//         break;    
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break; 
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("saturday");
+//         break;
+//     default:
+//         console.log("invalid day");      
+
+// }
+
+
+// // program for a simple calculator
+// let result;
+
+// // take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//         result = number1 + number2;
+//         console.log(`${number1} + ${number2} = ${result}`);
+//         break;
+//     case '-':
+//         result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         break;
+//     case '*':
+//         result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         break;
+//     case '/':
+//         result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+
+
+// multiple case
+
+// multiple case switch program
+let fruit = 'kj';
+switch(fruit) {
+    case 'apple':
+    case 'mango':
+    case 'pineapple':
+        console.log(`${fruit} is a fruit.`);
         break;
     default:
-        console.log("invalid day");      
-
+        console.log(`${fruit} is not a fruit.`);
+        break;
 }
