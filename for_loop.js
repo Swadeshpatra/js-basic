743cab9ab6c1f6a5d6cd71d01a0e44f716bbd04f
SWADESH <swadesh.patra961@gmail.com>
2022-12-05 10:27:43 +0530

    for loop

--- for_loop.js
+++ for_loop.js
@@ -10,3 +10,24 @@ const arr = [12,13,47,45,78];
 for(i = 0;i<=arr.length;i++){
     console.log(arr[i])
 };
+
+// program to display numbers from 1 to 5
+const k = 5;
+
+// looping from i = 1 to 5
+// in each iteration, i is increased by 1
+for (let i = 1; i <= k; i++) {
+    console.log(i);     // printing the value of i
+}
+
+
+let sum = 0;
+const j = 100
+
+// looping from i = 1 to n
+// in each iteration, i is increased by 1
+for (let i = 1; i <= j; i++) {
+    sum += i;   // sum = sum + i
+}
+
+console.log('sum =', sum,'.');
