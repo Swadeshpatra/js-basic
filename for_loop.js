f8f6214a5a763bfa67db1288dc502d02b9499d0f
SWADESH <swadesh.patra961@gmail.com>
2022-12-05 10:17:44 +0530

    for loop

---
+++ for_loop.js
@@ -0,0 +1,12 @@
+// program to display text 5 times
+const n = 5;
+
+// looping from i = 1 to 5
+for (let i = 1 ; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
+
+const arr = [12,13,47,45,78];
+for(i = 0;i<=arr.length;i++){
+    console.log(arr[i])
+};
