23570f1773cb2ef4d4cc7121c5f937a6999872a6
SWADESH <swadesh.patra961@gmail.com>
2022-12-05 10:49:07 +0530

    while loop

--- while_loop.js
+++ while_loop.js
@@ -4,3 +4,27 @@ while(i <=4){
     i++;
 }
 console.log('helo');
+
+// program to display numbers
+let e = 1;
+const n = 5;
+
+// do...while loop from 1 to 5
+do {
+    console.log(e);
+    e++;
+} while(e <= n);
+
+// to find the sum of positive numbers
+// if the user enters negative number, the loop terminates
+// negative number is not added to sum
+
+let sum = 0;
+let number = 0;
+
+do {
+    sum += number;
+    number = parseInt(prompt('Enter a number: '));
+} while(number >= 0)
+
+console.log(`The sum is ${sum}.`);
