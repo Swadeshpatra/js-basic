0b755bd5b4e9543c3faf8353d72e1f6bcac91492
SWADESH <swadesh.patra961@gmail.com>
2022-12-06 11:00:02 +0530

    function

---
+++ function.js
@@ -0,0 +1,18 @@
+// program to print a text
+// declaring a function
+function greet() {
+    console.log("Hello there!");
+}
+
+// calling the function
+greet();
+
+// program to add two numbers using a function
+// declaring a function
+function add(a, b) {
+    console.log(a + b);
+}
+
+// calling functions
+add(3,4);
+add(2,9);
