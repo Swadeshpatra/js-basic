676ef7b8727ac2455ef45d6a533422abc45367ed
Swadesh Patra <swadesh.patra961@gmail.com>
2023-01-24 21:33:46 +0530

    for loops new

--- for_loop.js
+++ for_loop.js
@@ -35,3 +35,22 @@ for (let i = 1; i <= j; i++) {
 }
 
 console.log('sum =', sum,'.');
+
+// for loops in arrays
+
+let fruits = ["mango","apple","grapes","oranges","banana"];
+console.log(fruits[0]);
+console.log(fruits[fruits.length-3]);
+for(let i=0; i < fruits.length-1; i++){
+    console.log(fruits[i].toUpperCase());
+}
+
+//  form a new arrays using above array
+
+let veg = ['onion','patato','kobi','gajar'];
+
+let veg2 = [];
+for(let i=0; i < veg.length; i++){
+    veg2.push(veg[i].toUpperCase());
+}
+console.log(veg2);
